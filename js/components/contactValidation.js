753224f13dddbef3cdff2ab94de5feb5cec16b95
ugniusnor <ugnn97@gmail.com>
2021-01-22 19:30:23 +0200

    up

--- js/components/contactValidation.js
+++ js/components/contactValidation.js
@@ -25,24 +25,32 @@ function validateInput() {
             document.querySelector(".err-name").style.display="block";
             document.querySelector(".err-name span").textContent="Name can't be empty"
             count++;
+        } else {
+            document.querySelector(".err-name").style.display="block";
         }
         if ( props.email.length < 2) {
             document.querySelector(".err-email").style.display="block";
             document.querySelector(".err-email span").textContent="Email can't be empty"
             count++;
 
+        } else  {
+            document.querySelector(".err-email").style.display="none";
         }
         if (!re.test(props.email)) {
             document.querySelector(".err-email").style.display="block";
             document.querySelector(".err-email span").textContent="It doesn't look like an email"
             count++;
-
+        }
+        else {
+            document.querySelector(".err-email").style.display="block";
         }
         if ( props.phone.length < 2) {
             document.querySelector(".err-phone").style.display="block";
             document.querySelector(".err-phone span").textContent="Phone can't be empty"
             count++;
-
+        }
+        {
+            document.querySelector(".err-phone").style.display="block";
         }
         if ( props.text.length < 2) {
             document.querySelector(".err-text").style.display="block";
@@ -50,6 +58,7 @@ function validateInput() {
             count++;
 
         }
+        document.querySelector(".err-text").style.display="block";
         if (count> 0) {
             count=0;
             return false;
